de95876f01b186e00812568534fa509aa7dc9e4e
Nicoding27 <nicolas.diaz@imagineapps.co>
2022-03-24 09:04:51 +0100

    Some changes added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,29 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Home from './routes/Home';
-import About from './routes/About';
-import Login from './routes/Login';
-
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import styled from "styled-components";
+import Login from "./routes/Login";
+import { AccountBox } from "./components /accountBox";
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to React Router!</h1>
-      <Routes>
+      <AppContainer>
+        <AccountBox />
+      </AppContainer>
+      {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="login" element={<Login />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
