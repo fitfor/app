df68f924e6043f633617acac3b5bdbfa9fe162cf
Matteo Agnoletto <30753195+EPMatt@users.noreply.github.com>
2022-03-25 06:25:08 +0000

    complete recommendations

--- frontend/src/components/RecommendationTile/index.tsx
+++ frontend/src/components/RecommendationTile/index.tsx
@@ -1,44 +1,60 @@
-import { Col, Row } from "react-bootstrap"
-import GridTile from "../GridTile"
+import { Col, Row } from "react-bootstrap";
+import GridTile from "../GridTile";
 
 const categoryColors = {
-    enduranceExercise: '#f79123',
-    flexibilityExercise: '#65c9cb',
-    focusExercise: '#e30713',
-    healthyEating: '#71c065',
-    motivationExercise: '#3acb80',
-    relaxationExercise: '#6558a8',
-    sleepingExercise: '#4e73bf',
-    strengthExercise: '#ed4340',
-    weightTrainingExercise: '#da3313',
-}
+  "Endurance excercise": "#f79123",
+  "Flexibility excercise": "#65c9cb",
+  "Focus excercise": "#e30713",
+  "HealthyEating ": "#71c065",
+  "Motivation excercise": "#3acb80",
+  "Relaxation excercise": "#6558a8",
+  "Sleeping excercise": "#4e73bf",
+  "Strength excercise": "#ed4340",
+  "Weighttraining excercise": "#da3313",
+};
 
-type CategoryIDs = 'enduranceExercise' | 'flexibilityExercise' | 'focusExercise' | 'healthyEating' | 'motivationExercise' | 'relaxationExercise' |
-    'sleepingExercise' |
-    'strengthExercise' |
-    'weightTrainingExercise'
-
+type CategoryIDs =
+  | "Endurance excercise"
+  | "Flexibility excercise"
+  | "Focus excercise"
+  | "HealthyEating "
+  | "Motivation excercise"
+  | "Relaxation excercise"
+  | "Sleeping excercise"
+  | "Strength excercise"
+  | "Weighttraining excercise";
 
 const categoryNames = {
-    enduranceExercise: '#f79123',
-    flexibilityExercise: '#65c9cb',
-    focusExercise: '#e30713',
-    healthyEating: '#71c065',
-    motivationExercise: '#3acb80',
-    relaxationExercise: '#6558a8',
-    sleepingExercise: '#4e73bf',
-    strengthExercise: '#ed4340',
-    weightTrainingExercise: '#da3313',
-}
+    "Endurance excercise": "Endurance",
+    "Flexibility excercise": "Flexibility",
+    "Focus excercise": "Focusing",
+    "HealthyEating ": "Healthy Eating",
+    "Motivation excercise": "Motivation",
+    "Relaxation excercise": "Relaxation",
+    "Sleeping excercise": "Sleeping",
+    "Strength excercise": "Strength",
+    "Weighttraining excercise": "Weight Training",
+  };
 
 function RecommendationTile(props: any) {
-    const categoryId = props.categoryId as CategoryIDs
+  const categoryId = props.categoryId as CategoryIDs;
 
-    return (
-        <GridTile {...props} style={{ minHeight: 100 }} backgroundColor={categoryColors[categoryId]} opacityHex={40}>
-            <Row className='h-100 align-items-end'><Col style={{}}><h4 style={{ fontWeight: 800, textAlign: 'right', color: 'white' }}>{categoryNames[categoryId]}</h4></Col></Row>
-        </GridTile>
-    )
+  return (
+    <GridTile
+      {...props}
+      style={{ minHeight: 100 }}
+      backgroundColor={categoryColors[categoryId]}
+      opacityHex={40}
+    >
+      <Row className="h-100 align-items-end">
+        <Col style={{}}>
+          <h4 style={{ fontWeight: 800, textAlign: "right", color: "white" }}>
+            {categoryNames[categoryId]}
+          </h4>
+        </Col>
+      </Row>
+    </GridTile>
+  );
 }
 
-export default RecommendationTile
+export default RecommendationTile;
--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
 import AvatarImage from "../components/AvatarImage";
 import { withBottomNavigation } from "../components/BottomNavigation";
@@ -5,21 +6,38 @@ import PageTitle from "../components/PageTitle";
 import RecommendationTile from "../components/RecommendationTile";
 
 function Home() {
-    return (
-        <Container >
-            <PageTitle style={{textAlign:'center'}}>Welcome back, John!</PageTitle>
-            <Row className='' style={{ alignItems: 'center', flexDirection: 'column', textAlign: 'center'}}>
-                <AvatarImage src='https://source.unsplash.com/random/300x300' />
-                <p className='mt-5'>You can do this, I believe in you.</p>
-            </Row>
-            <Row xs={2} justifyContent='center'>
-                <RecommendationTile categoryId='enduranceExercise'></RecommendationTile>
-                <RecommendationTile categoryId='focusExercise'></RecommendationTile>
-                <RecommendationTile categoryId='sleepingExercise'></RecommendationTile>
-                <RecommendationTile categoryId='healthyEating'></RecommendationTile>
-            </Row>
-        </Container>
-    );
+  const [recommendation, setRecommendation] = useState([]);
+  useEffect(() => {
+    const axios = require("axios").default;
+
+    axios({
+      method: "get",
+      url: "https://fitfor.herokuapp.com/recommendations/",
+    }).then((response: any) => {console.log(response.data.consequent); setRecommendation(response.data.consequent)});
+  }, []);
+
+  const recCards = (recommendation).map((k: any) => (
+    <RecommendationTile key={k} categoryId={k}></RecommendationTile>
+  ));
+  return (
+    <Container>
+      <PageTitle style={{ textAlign: "center" }}>Welcome back, John!</PageTitle>
+      <Row
+        className=""
+        style={{
+          alignItems: "center",
+          flexDirection: "column",
+          textAlign: "center",
+        }}
+      >
+        <AvatarImage src="https://source.unsplash.com/random/300x300" />
+        <p className="mt-5">You can do this, I believe in you.</p>
+      </Row>
+      <Row xs={2} justifyContent="center">
+        {recCards}
+      </Row>
+    </Container>
+  );
 }
 
-export default withBottomNavigation(Home)
+export default withBottomNavigation(Home);
