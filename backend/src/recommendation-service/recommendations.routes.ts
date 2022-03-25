f97a936d598dcabdafc2ca47f24bda09d6b8b726
Matteo Agnoletto <30753195+EPMatt@users.noreply.github.com>
2022-03-25 05:33:00 +0000

    get recommendations from the backend

--- backend/src/recommendation-service/recommendations.routes.ts
+++ backend/src/recommendation-service/recommendations.routes.ts
@@ -1,37 +1,31 @@
-import express from 'express'
-const router = express.Router()
+import express from "express";
+const router = express.Router();
 
 const obj = {
-  'act1': 'Focus excercise',
-  'act2': 'Sleeping excercise',
-  'act3': 'Flexibility excercise',
-  'act4': '',
-  'act5': '',
-  'act6': '',
-  'act7': '',
-  'act8': '',
+  act1: "Focus excercise",
+  act2: "Sleeping excercise",
+  act3: "Flexibility excercise",
+  act4: "",
+  act5: "",
+  act6: "",
+  act7: "",
+  act8: "",
 };
 
-router.get('/', callGet_recommendations);
+router.get("/", callGet_recommendations);
 
-function callGet_recommendations(req:any, res:any) {
-  // using spawn instead of exec, prefer a stream over a buffer
-  // to avoid maxBuffer issue
-  console.log('here')
-  var spawn = require('child_process').spawn;
-  var process = spawn('python', ['./get_recommendations.py',
-    obj.act1,
-    obj.act2,
-    obj.act3,
-    obj.act4,
-    obj.act5,
-    obj.act6,
-    obj.act7,
-    obj.act8
-  ]);  process.stdout.on('data', function (data:any) {
-    res.send(data.toString());
+async function callGet_recommendations(req: any, res: any) {
+  const axios = require("axios").default;
+
+  const response = await axios({
+    method: "post",
+    url: "https://recomm-microservice.herokuapp.com/get-recomm/",
+    data: {
+      obj,
+    },
   });
+
+  res.send(response.data);
 }
 
-
-export default router
+export default router;
